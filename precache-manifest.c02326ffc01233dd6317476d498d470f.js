81eb7602a326fa45fabec59013ebba0b0e31c884
Katie Ingham <kjingham91@gmail.com>
2020-03-11 14:00:49 +0000

    Updates

---
+++ precache-manifest.c02326ffc01233dd6317476d498d470f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bb84bb779fe910b8f4172835d402b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39cc7e4f30189b67b2d8",
+    "url": "/static/css/main.23f37c70.chunk.css"
+  },
+  {
+    "revision": "f388e8074e1f6ce76fc1",
+    "url": "/static/js/2.fe21fc44.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.fe21fc44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39cc7e4f30189b67b2d8",
+    "url": "/static/js/main.44ec78ec.chunk.js"
+  },
+  {
+    "revision": "7aff21087884eee70168",
+    "url": "/static/js/runtime-main.7efaf76e.js"
+  }
+]);
